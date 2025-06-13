493ef6aec313477dd8108e2eb67f07922225be61
Monicah Koina <monicahkoina1@gmail.com>
2025-06-13 16:05:28 +0300

    use he window object for navigation

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 function showModal() {
-    const modalHTML = `
+  const modalHTML = `
       <input class="modal-state" id="modal-1" type="checkbox">
       <div class="modal">
         <label class="modal-bg" for="modal-1"></label>
         <div class="modal-body">  
           <h4 class="modal-title">What would you like to create?</h4>
           <div class="flex flex-col gap-2 my-4">
-            <button class="btn-secondary-outline "><a href="#">Daily Routine</a></button>
-            <button class="btn-secondary-outline"><a href="#">Meal Plan</a></button>
-            <button class="btn-secondary-outline"><a href="#">Budget Plan</a></button>
-            <button class="btn-secondary-outline"><a href="#">Study Plan</a></button>
+        <button onclick="window.location.href='/dailyRoutine/index.html'" class="btn-secondary-outline">Daily routine</button>
+            <button onClick="window.location.href='/mealPlan/index.html'"  class="btn-secondary-outline">Meal Plan</button>
+            <button  onClick="window.location.href='/budgetPlan/index.html'"class="btn-secondary-outline">Budget Plan</button>
+            <button onClick="window.location.href='/studyPlan/index.html'" class="btn-secondary-outline">Study Plan</button>
           </div>
           <label for="modal-1" class="btn-secondary mt-4">Cancel</label>
         </div>
       </div>
     `;
-    document.getElementById("modal-container").innerHTML = modalHTML;
-    setTimeout(() => {
-        document.getElementById("modal-1").checked = true;
-    }, 10);
+  document.getElementById("modal-container").innerHTML = modalHTML;
+  setTimeout(() => {
+    document.getElementById("modal-1").checked = true;
+  }, 10);
 }
