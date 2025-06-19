aa867690c42ddb7692a0dffe8a04df686ae0a82b
Monicah Koina <monicahkoina1@gmail.com>
2025-06-20 00:12:40 +0300

    custom attributes

--- mealPlan/meal.js
+++ mealPlan/meal.js
@@ -7,23 +7,42 @@ let formElem = document.getElementById("mealForm");
 formElem.addEventListener("submit", function (e) {
     e.preventDefault();
     let day = formElem.day.value;
-    let type = formElem.type.value;
-    let food = formElem.food.value;
+    let meal = formElem.meal.value;
+    let food = formElem.food.value.trim();
     if (!food) {
         return;
     }
-
-    let cards = document.querySelectorAll(".card");
-    cards.forEach(card => {
+    // let cards = document.querySelectorAll(".card")
+    let matchingCard = [...document.querySelectorAll(".card")].find(card => {
         let h1 = card.querySelector("h1");
-        let dayAttr = h1.dataset.day;
-        // console.log(dayAttr);
-        let table = card.querySelector(".meal-table")
-        let th = document.createElement("th")
-        th.appendChild(table)
-        th.innerHTML = day;
-        th.innerHTML = type;
-        th.innerHTML = food;
-    })
+        return h1.dataset.day === day;
+    });
+    if (matchingCard) {
+
+        let ths = matchingCard.querySelectorAll("th")
+        let targetTH = [...ths].find(th => th.dataset.meal === meal);
+        let table = matchingCard.querySelector(".meal-table")
+        let tbody = document.createElement("tbody")
+        let tr = document.createElement("tr")
+        tbody.appendChild(tr);
+        table.appendChild(tbody);
+
+        if (targetTH) {
+            let td1 = document.createElement("td")
+            td1.innerHTML = food
+            tr.appendChild(td1);
+            let td2 = document.createElement("td")
+            td2.innerHTML = food
+            tr.appendChild(td2);
+            let td3 = document.createElement("td")
+            td3.innerHTML = food
+            tr.appendChild(td3);
+        }
+
+
+
+
+    }
+
 }
 )
